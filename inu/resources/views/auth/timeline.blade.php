6420ff7485420bce40a50fae4e08f17ed9dae759
200635sawa <200635@jc-21.jp>
2022-02-28 13:49:38 +0900

    マイグレーション

--- inu/resources/views/auth/timeline.blade.php
+++ inu/resources/views/auth/timeline.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>柴犬SNS</title>
+    <title>shibastagram</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body style="height:100%; background-color: #E6ECF0;">
