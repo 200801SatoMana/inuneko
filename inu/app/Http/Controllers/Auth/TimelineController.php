7d1efb2429f8626fbe6fa900fbf70c4bf8e0383c
200635sawa <200635@jc-21.jp>
2022-02-28 13:38:02 +0900

    TL実装

--- inu/app/Http/Controllers/Auth/TimelineController.php
+++ inu/app/Http/Controllers/Auth/TimelineController.php
@@ -2,18 +2,18 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth; // <--- 追加
-use \App\Tweet; // <--- 追加
+use \App\Models\tweet; // <--- 追加
 
 
 class TimelineController extends Controller
 {
     public function showTimelinePage()
     {
-        $tweets = Tweet::latest()->get();  // <--- 追加
-        return view('auth.timeline', compact('tweets'));   // <--- 変更
+        $tweets = tweet::latest()->get();  // <--- 追加
+        return view('auth.timeline',compact('tweets'));   // <--- 変更
     }
 
     public function postTweet(Request $request) //ここはあとで実装します。(Requestはpostリクエストを取得するためのものです。)
@@ -22,7 +22,7 @@ class TimelineController extends Controller
         $validator = $request->validate([
             'tweet' => ['required', 'string', 'max:280'],
         ]);
-        Tweet::create([
+        tweet::create([
             'user_id' => Auth::user()->id,
             'tweet' => $request->tweet,
         ]);
